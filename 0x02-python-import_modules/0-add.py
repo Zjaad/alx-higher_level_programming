506d4876d65f30fe9f544cb1d093f02cada86181
zjaad <zjaadani@gmail.com>
2023-08-11 17:29:47 -0700

    adding first file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-def main():
-    """Print the sum of 1 and 2."""
+if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    if __name__ == "__main__":
-        main()
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
