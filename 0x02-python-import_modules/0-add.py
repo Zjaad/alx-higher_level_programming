d2292ad706117ad0fffcea26515ef8945ec52420
zjaad <zjaadani@gmail.com>
2023-08-11 17:26:55 -0700

    adding first file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,6 +5,6 @@ def main():
     from add_0 import add
     a = 1
     b = 2
+    if __name__ == "__main__":
+        main()
     print("{} + {} = {}".format(a, b, add(a, b)))
-if __name__ == "__main__":
-    main()
