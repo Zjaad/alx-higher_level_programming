3d7d52a842c8c4e743c4490f0517df81072b6726
zjaad <zjaadani@gmail.com>
2023-08-16 14:43:28 -0700

    correcting first file

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    alist = []
-    for x in my_list:
-        if x == search:
-            alist.append(replace)
-        else:
-            alist.append(x)
-            return alist
+    alist = [replace if x == search else x for x in my_list]
+    return alist
