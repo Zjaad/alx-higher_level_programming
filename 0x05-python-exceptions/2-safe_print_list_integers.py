602b9f072bd5a861b0fa8471dd0edd8211765bc9
zjaad <zjaadani@gmail.com>
2023-08-28 12:10:04 -0700

    adding files

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
     pe = 0 
-    for y in range(0, x):
+    for y in range(0,min(x, len(my_list))):
         try :
-             print("{:d}".format(my_list[y]), end="")
-             pe += 1 
+            print("{:d}".format(my_list[y]), end="")
+            pe += 1 
         except (ValueError, TypeError):
             continue
     print()
