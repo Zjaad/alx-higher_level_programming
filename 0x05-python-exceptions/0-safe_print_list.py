fc1bd0e17fdd9a6579ae537e0b15d376fca13010
zjaad <zjaadani@gmail.com>
2023-08-28 08:05:52 -0700

    fist file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     pe = 0
-    try:
-        for y in range(x):
-             print("{}".format(my_list[y]), end="")
+    y = 0
+    for y in range(0, x):
+        try:
+            print("{}".format(my_list[y]), end="")
              pe += 1
-    except:
-        pass
+        except:
+            continue
     print()
     return (pe)
