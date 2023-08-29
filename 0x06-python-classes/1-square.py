b88313573f8cfc0f9ed39772675cc0f2c0570768
zjaad <zjaadani@gmail.com>
2023-08-29 14:53:57 -0700

    fixing the ind

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 
+"""
+Module Documentation: 1-square
+
+This module created to define the Square class.
+"""
+
 class Square:
+    """
+    Square class with private instance attribute the size
+    """
     def __init__(self, size):
+        """
+        Intializing the Square instance.
+
+        args:
+            size (int): The Size of the Square.
+
+        Attributes:
+            __size (int): The private instance that represent the size.
+        """
         self.__size = size
