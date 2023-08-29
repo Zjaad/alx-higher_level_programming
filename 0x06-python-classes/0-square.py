a195cc8d5699407b3d2f189b531bea6033391bd0
zjaad <zjaadani@gmail.com>
2023-08-29 14:45:24 -0700

    adding files

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+
+""" Module Documentation : 0-Square
+
+This module created to define the square class
+"""
+
 class Square:
+    """
+    Empty Class named Square
+    """
     pass
