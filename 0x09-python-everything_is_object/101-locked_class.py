149eaf0d42485d9b2a82754df4bc4a43b7f5d1b1
zjaad <zjaadani@gmail.com>
2023-09-05 18:42:04 -0700

    adding all files

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,3 +2,7 @@
 """ Defines the class LockedClass."""
 class LockedClass:
     __slots__ = ('first_name',)
+
+    def __init__(sl):
+        """ The new instances of locked class """
+        sl.first_name = "first_name"
