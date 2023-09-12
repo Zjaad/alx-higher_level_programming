2cf2885ca906dc3b33b120715621e6750de297fa
zjaad <zjaadani@gmail.com>
2023-09-11 18:11:53 -0700

    addingfiles

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -10,4 +10,6 @@ def is_kind_of_class(obj, a_class):
     Returns:
         True if obj is an instance of, or inherited from, a_class; otherwise, False.
     """
-    return isinstance(obj, a_class)
+    if isinstance(obj, a_class):
+        return (True)
+    return (False)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines empty class BaseGeometry."""
+
+class BaseGeometry:
+    """Represents base geometry."""
+    pass
