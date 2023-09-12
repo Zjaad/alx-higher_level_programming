7cfbeb4241da2991e7e5284282fe76eded88ae63
zjaad <zjaadani@gmail.com>
2023-09-11 17:30:10 -0700

    adding files

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Task numtwo"""
+
+class MyList(list):
+    """Enables sorted printing for instances of the built-in list class."""
+
+    def print_sorted(self):
+        """Prints a list in sorted ascending order."""
+        print(sorted(self))
