57c12bd807b5f7188cfcf5138055943ba8bfc686
zjaad <zjaadani@gmail.com>
2023-09-12 19:31:04 -0700

    cf

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,6 @@
 """Function that reads a text UTF8 and prints it."""
 def read_file(filename=""):
     """Function read_file.""" 
-    with open(filename, encoding='utf-8') as fi:
-        print(fi.read(), end= "")       
+    with open(filename) as fi:
+        rt = fi.read() 
+        print(rt, end="")       
