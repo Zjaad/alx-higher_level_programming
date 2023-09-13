80ba7e37ffbf87292f4b9fadb1657633473e0563
zjaad <zjaadani@gmail.com>
2023-09-12 18:00:04 -0700

    adding file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """ a function that reads a text file (UTF8) and prints it """
 def read_file(filename=""):
-    """ function read_file """
-    try:
-        with open(filename, encoding='utf-8') as file:
-            print(file.read(), end= "")
-    except FileNotFoundError:
-        pass
+    """ function read_file """ 
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end= "")       
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a function that writes a string to a text file (UTF8) and returns the number of characters written"""
+
+
+def write_file(filename="", text=""):
+    """the function that write string to UTF8 text.
+
+    Args:
+        filename (str): The file to write.
+        text (str): To write to the file.
+    Returns:
+        The number of characters.
+    """
+    with open(filename, "w", encoding="utf-8") as fl:
+        return (fl.write(text))
