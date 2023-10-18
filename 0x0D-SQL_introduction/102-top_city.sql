3dbd5a2fdeffc7fee147b123eb7a4b1f22fa96c3
zjaad <zjaadani@gmail.com>
2023-10-17 17:15:29 -0700

    cf

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- display cities with the highest avg temperatures
-SELECT city, AVG(value) AS avg_temp
-FROM temperatures
-WHERE MONTH(date) IN (7, 8)
-GROUP BY city
-ORDER BY avg_temp DESC
+SELECT `city`, AVG(`temperature`) AS `avg_temp`
+FROM `temperatures`
+WHERE MONTH(`date`) = 7 OR MONTH(`date`) = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
 LIMIT 3;
