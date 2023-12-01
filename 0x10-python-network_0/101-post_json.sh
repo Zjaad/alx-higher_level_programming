b2376ab4bdd269ca9dd60601fe63e09b4b1684c4
zjaad <zjaadani@gmail.com>
2023-11-30 17:28:03 -0800

    another commit

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 #Script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-
-url="$1"
-jsonFile="$2"
-curl -sX POST "$url" -H "Content-Type: application/json" -d @"$jsonFile" -L
+curl -sX POST $1 -H "Content-Type: application/json" -d @$2 -L
