346e1796e35616b011150eeec4a5210b4ecc01bf
zjaad <zjaadani@gmail.com>
2023-12-01 16:27:10 -0800

    another commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         owner = argv[1]
         repository = argv[2]
         link = f'https://api.github.com/repos/{owner}/{repository}/commits'
-
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
         try:
             r = requests.get(link)
             r.raise_for_status()
