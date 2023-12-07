d31aed2990a0ccab0fec718f2c8c11fdcb94cd6a
Zjaad <127573785+Zjaad@users.noreply.github.com>
2023-12-07 23:03:23 +0000

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++
@@ -1,6 +0,0 @@
--- lists all genres of the show Dexter.
-SELECT name FROM tv_genres
-JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
-JOIN tv_show_genres ON id=tv_show_genres.genre_id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY name;
--- 0x0E-SQL_more_queries/6-states.sql
+++
@@ -1,8 +0,0 @@
--- Creates the database hbtn_0d_usa and the table states
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-	id INT AUTO_INCREMENT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id),
-	UNIQUE KEY (id)
-);
