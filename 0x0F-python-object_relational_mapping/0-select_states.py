a7e67b1fd484de56660d1d7addde743cf0fc492e
zjaad <zjaadani@gmail.com>
2024-02-22 16:15:46 -0800

    editing files

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-import MySQLdb
 import sys
+import MySQLdb
 
-if __name__ == "__main__":
-    datab = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+def safe_filter_states(username, password, dbname, state_name):
+    """Safely display all states matching the user-provided name."""
+    datab = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=dbname)
     c = datab.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id` ASC")
+    c.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,))
     r = c.fetchall()
     for ir in r:
         print(ir)
     c.close()
     datab.close()
+
+if __name__ == "__main__":
+    safe_filter_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
